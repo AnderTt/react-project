2b6247ce22f898a8a1ce5dee75ad0f507c9177e2
tongjuan <vstj007@126.com>
2018-08-11 15:06:20 +0800

    update redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter,Switch,Route} from 'react-router-dom'
+import {HashRouter,Switch,Route} from 'react-router-dom';
+import {Provider} from 'react-redux'
 
 import login from './containers/login/Login'
 import register from './containers/register/Register'
 import main from './containers/main/Main'
+import store from './redux/store'
 
 ReactDOM.render(
-  <HashRouter>
-    <Switch>
-      <Route path='./login' component={login} />
-      <Route path='./register' component={register} />
-      <Route component={main} />
-    </Switch>
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <Switch>
+        <Route path='./login' component={login} />
+        <Route path='./register' component={register} />
+        <Route component={main} />
+      </Switch>
+    </HashRouter>
+  </Provider>,
   document.getElementById('root'));
